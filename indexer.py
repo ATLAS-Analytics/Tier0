45957b888ed2bb45a7a4cfb580faeee2e806fc4b
Ilija Vukotic <ivukotic@cern.ch>
2020-07-21 23:18:43 -0500

    adding pipeline

--- indexer.py
+++ indexer.py
@@ -68,6 +68,7 @@ for row in cursor:
     if doc['MTIME']:
         doc['MTIME'] = str(doc['MTIME']).replace(' ', 'T')
     doc["_index"] = "t0_tasks"
+    doc["pipeline"] = "t0_tasks"
     doc["_id"] = doc['TASKID']
 
     data.append(doc)
